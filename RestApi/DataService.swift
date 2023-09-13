a0891878f97b3eff532f63da463b824a5f1c750e
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-09-13 10:14:33 +0100

    final version of CRUD operations

--- RestApi/DataService.swift
+++ RestApi/DataService.swift
@@ -69,14 +69,7 @@ class DataService {
         var postRequest = URLRequest(url: validURL)
         postRequest.httpMethod = "POST"
         
-        let authString = "TOKEN_API"
-        var authStringBase64 = ""
-        
-        // Basic auth (user, password)
-        if let authData = authString.data(using: .utf8){
-            authStringBase64 = authData.base64EncodedString()
-        }
-        postRequest.setValue("Basic \(authStringBase64)", forHTTPHeaderField: "Authorization")
+        postRequest.setValue("Basic \(createAuthCredentials())", forHTTPHeaderField: "Authorization")
         postRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
         postRequest.setValue("application/json", forHTTPHeaderField: "Accept")
         
@@ -115,6 +108,106 @@ class DataService {
                 completion(.failure(serializationError))
             }
         }.resume()
+    }
+    
+    //PUT
+    func updateGist(id: String, completion: @escaping (Result<Any,Error>) -> Void){
+        let updateComponent = createURLComponents(path: "/gists/\(id)")
+        // operator similar to IF codition (example : if validURL = componentURL.url is invalid the else block invoked)
+        guard let validURL = updateComponent.url else {
+            print("URL creation failed ... ")
+            return // End of function fetchGists()
+        }
+        
+        var updateRequest = URLRequest(url: validURL)
+        updateRequest.httpMethod = "PUT"
+        
+        updateRequest.setValue("Basic \(createAuthCredentials())", forHTTPHeaderField: "Authorization")
+        updateRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        updateRequest.setValue("application/json", forHTTPHeaderField: "Accept")
+        
+        // Create new Gist for test
+        let updatedGist = Gist(id: nil, isPublic: false, description: "A brand update Gist", files: ["text_file.txt": File(content: "Hello World")])
+        
+        do{
+            // try to encode Gist to JSON
+            let gistData = try JSONEncoder().encode(updatedGist)
+            // add JSON data to the HTTP Post request
+            updateRequest.httpBody = gistData
+        }catch {
+            print("Gist encoding failed ...")
+        }
+        
+        URLSession.shared.dataTask(with: updateRequest){ (data, response, error) in
+            if let httpResponse = response as? HTTPURLResponse { // as : convert to HTTPURLResponse
+                print("API status : \(httpResponse.statusCode)") // HTTP (Status) example : 200/201 = OK, 404: host not find, 500 error in server .... 401, 403 : permession ..
+            }
+            
+            // get data from httpResponse
+            guard let validData = data, error == nil else {
+                completion(.failure(error!))
+                return
+            }
+            
+            do {
+                let gists = try JSONDecoder().decode([Gist].self, from: validData)
+                completion(.success(gists))
+            }catch let serializationError {
+                completion(.failure(serializationError))
+            }
+        }
+        
+        
+    }
+    
+    //DELETE
+    func deleteGist(id: String, star:Bool, completion: @escaping (Result<Any,Error>) -> Void){
+        let deleteComponent = createURLComponents(path: "/gists/\(id)")
+        // operator similar to IF codition (example : if validURL = componentURL.url is invalid the else block invoked)
+        guard let validURL = deleteComponent.url else {
+            print("URL creation failed ... ")
+            return // End of function fetchGists()
+        }
+        
+        var deleteRequest = URLRequest(url: validURL)
+        deleteRequest.httpMethod = "DELETE"
+        
+        deleteRequest.setValue("Basic \(createAuthCredentials())", forHTTPHeaderField: "Authorization")
+        deleteRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        deleteRequest.setValue("application/json", forHTTPHeaderField: "Accept")
+ 
+        
+        URLSession.shared.dataTask(with: deleteRequest){ (data, response, error) in
+            if let httpResponse = response as? HTTPURLResponse { // as : convert to HTTPURLResponse
+                print("API status : \(httpResponse.statusCode)") // HTTP (Status) example : 200/201 = OK, 404: host not find, 500 error in server .... 401, 403 : permession ..
+            }
+            
+            // get data from httpResponse
+            guard let validData = data, error == nil else {
+                completion(.failure(error!))
+                return
+            }
+            
+            do {
+                let gists = try JSONDecoder().decode([Gist].self, from: validData)
+                completion(.success(gists))
+            }catch let serializationError {
+                completion(.failure(serializationError))
+            }
+        }
+        
+        
+    }
+    
+    func createAuthCredentials() -> String {
+        let authString = "TOKEN_API"
+        var authStringBase64 = ""
+        
+        // Basic auth (user, password)
+        if let authData = authString.data(using: .utf8){
+            authStringBase64 = authData.base64EncodedString()
+        }
+        return authStringBase64
     }
     
     // Générer le PATH de la requette HTTP
